00e347dfa31a27db4cbfca6520bcfa254442445a
breaknow <breaknow@gmail.com>
2018-08-06 21:24:49 +0900

    heap finish

--- epi_judge_cpp/k_largest_in_heap.cc
+++ epi_judge_cpp/k_largest_in_heap.cc
@@ -1,10 +1,32 @@
 #include <vector>
+#include <queue>
 #include "test_framework/generic_test.h"
 using std::vector;
+using namespace std;
+
+struct HeapEntry{
+  int val, pos;
+  bool operator<(const HeapEntry &rhs) const {
+    return val < rhs.val;
+  }
+};
 
 vector<int> KLargestInBinaryHeap(const vector<int>& A, int k) {
-  // TODO - you fill in here.
-  return {};
+  priority_queue<HeapEntry, vector<HeapEntry>, less<>> max_heap;
+  vector<int> ans;
+  if(A.empty())
+    return ans;
+  max_heap.emplace(HeapEntry{A[0], 0});
+  for(int i=0;i<min(k, (int)A.size());i++){
+    HeapEntry cur(max_heap.top());
+    max_heap.pop();
+    ans.emplace_back(cur.val);
+    if(cur.pos * 2+1 < A.size()) // start from 0
+		max_heap.emplace(HeapEntry{A[cur.pos * 2+1], cur.pos * 2+1});
+	if(cur.pos * 2 + 2< A.size())
+    	max_heap.emplace(HeapEntry{A[cur.pos * 2 + 2], cur.pos * 2+2});
+  }
+  return ans;
 }
 
 int main(int argc, char* argv[]) {
--- epi_judge_cpp/sort_almost_sorted_array.cc
+++ epi_judge_cpp/sort_almost_sorted_array.cc
@@ -1,11 +1,28 @@
 #include <vector>
+#include <queue>
 #include "test_framework/generic_test.h"
 using std::vector;
+using namespace std;
+
 vector<int> SortApproximatelySortedData(
     vector<int>::const_iterator sequence_begin,
     const vector<int>::const_iterator& sequence_end, int k) {
-  // TODO - you fill in here.
-  return {};
+  priority_queue<int, vector<int>, greater<>> min_heap;
+  vector<int>ans;
+  if(sequence_begin == sequence_end)
+    return ans;
+  while(sequence_begin != sequence_end){
+    if(min_heap.size() > 2 * k){
+      ans.emplace_back(min_heap.top());
+      min_heap.pop();
+    }
+    min_heap.emplace(*sequence_begin++);
+  }
+  while(!min_heap.empty()){
+    ans.emplace_back(min_heap.top());
+    min_heap.pop();
+  }
+  return ans;
 }
 vector<int> SortApproximatelySortedDataWrapper(const vector<int>& sequence,
                                                int k) {
